be7fad2bb060771e81e55db1539154080f7939a4
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-05 03:26:34 +0330

    Working on the crawling process

--- crawler/main.py
+++ crawler/main.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+url = os.getenv("BASE_URL")
+res = requests.get(url)
+data = BeautifulSoup(res.text, 'lxml')
+data = data.find_all('div', class_="flw-item")
+
+print(len(data))
