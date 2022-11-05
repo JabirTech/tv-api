6d5ef21489b1a25d9b99b3aed4a823d32cc2adf7
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-05 03:46:02 +0330

    Trying to get better results

--- crawler/main.py
+++ crawler/main.py
@@ -15,11 +15,14 @@ for datum in data:
     image = datum.img['data-src']
     watch_link = datum.a['href']
 
-    connection = sqlite3.connect(os.path.join(os.getcwd(), 'db.sqlite3'))
-    cur = connection.cursor()
-    query = f"INSERT INTO movies VALUES ('{name}', '{image}', '{watch_link}')"
-    cur.execute(query)
-    cur.close()
-    connection.commit()
+    try:
+        connection = sqlite3.connect(os.path.join(os.getcwd(), 'db.sqlite3'))
+        cur = connection.cursor()
+        query = f"INSERT INTO movies VALUES ('{name}', '{image}', '{watch_link}')"
+        cur.execute(query)
+        cur.close()
+        connection.commit()
+    except Exception as e:
+        print(e)
     
     print(f'{name} added to the DB')
