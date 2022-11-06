13c3ea181e72f0e47343a9fc05a88a778ee606d8
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-07 02:52:27 +0330

    Working on movie list and stuff

--- api.py
+++ api.py
@@ -1,3 +1,14 @@
 from flask import Flask, request
 import os
 import sqlite3
+
+def get_movie_lists():
+    connection = sqlite3.connect(os.path.join(os.getcwd(), 'crawler/db.sqlite3'))
+    curr = connection.cursor()
+    query = "SELECT * from movies"
+    res = curr.execute(query)
+    res = res.fetchall()
+    curr.close()
+    connection.commit()
+
+    return res
